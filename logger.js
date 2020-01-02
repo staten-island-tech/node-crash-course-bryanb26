974b65c5a902f349d74363e6999441d942373bd0
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2020-01-01 22:22:14 -0500

    silly hw

---
+++ logger.js
@@ -0,0 +1,15 @@
+const EventEmitter = require('events');
+const uuid = require('uuid');
+
+class Logger extends EventEmitter {
+    log(msg) {
+        // Call Event
+        this.emit('message', { id: uuid.v4(), msg});
+    }
+}
+
+///module.exports = Logger;
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
